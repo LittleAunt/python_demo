a75e1c287d18ceea3cd5c177c38a98208c452484
李二白 <lierbai@lierbaideMacBook-Pro.local>
2021-12-08 23:42:50 +0800

    first test

---
+++ 1_urlopen.py
@@ -0,0 +1,9 @@
+from urllib.request import urlopen
+ 
+url = "http://www.baidu.com"
+resp = urlopen(url)
+# print(resp.read().decode("utf-8")) #字节转字符串输出
+#打开文件，输出
+with open("mybaidu.html", mode="w") as f:
+    f.write(resp.read().decode("utf-8"))
+    print("over!")
